d16008d66e0dda1407062966517d12b293161231
root <root@Debian-Linsmar>
2016-11-28 22:38:25 -0300

    Web service begin

---
+++ src/main/java/com/linsmar/EmprestBankApplication.java
@@ -0,0 +1,12 @@
+package com.linsmar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmprestBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmprestBankApplication.class, args);
+	}
+}
